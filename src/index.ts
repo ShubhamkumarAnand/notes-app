a31a2c9bd7fc684b7bf6ba406814d72b65b1f42a
imskanand <the.anand3099@gmail.com>
2024-01-04 23:54:04 +0530

    + Search Functionality is working

--- src/index.ts
+++ src/index.ts
@@ -36,11 +36,33 @@ app.route("/auth", auth);
 
 app.route("", notes);
 
-// app.get("/search", async (c) => {
-//   const query = c.req.query("q");
-//   const notes = await findNote(query);
-//   if (!notes) return c.json({ message: "Note Not Found!" });
-//   return c.json({ notes });
-// });
+// search middleware
+app.use("/search/*", async (c, next) => {
+  const bearer = c.req.header().authorization;
+  if (!bearer) return c.json({ message: "Not Authorized" }, 401);
+
+  const [, token] = bearer.split(" ");
+  if (!token) return c.json({ message: "Not a valid token!" }, 401);
+
+  try {
+    const user = await verify(token, process.env.JWT_SECRET as string);
+
+    // Setup the request header
+    c.req.raw.headers.set("userId", user.id);
+
+    await next();
+  } catch (error) {
+    console.log(error);
+    return c.json({ error });
+  }
+});
+
+app.get("/search", async (c) => {
+  const query = c.req.query("q");
+  const userData = (await c.req.raw.headers.get("userId")) as string;
+  const notes = await findNote(query, userData);
+  if (!notes) return c.json({ message: "Note Not Found!" });
+  return c.json({ notes });
+});
 
 export default app;
